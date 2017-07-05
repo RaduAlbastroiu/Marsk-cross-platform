a5673464b4e018da36b4750a9a158d342c08dd10
Albastroiu Radu <albastroiuradug@gmail.com>
2017-07-05 22:59:04 +0300

    Hero proiectile class added
    - now the proiectiles will be managed by a proiectile class which will
    contain multiple types of proiectiles like hero proiectile, this is the
    first type

---
+++ HeroProiectile.cpp
@@ -0,0 +1,24 @@
+//
+//  HeroProiectile.cpp
+//  Marsk
+//
+//  Created by Albastroiu Radu on 7/5/17.
+//
+//
+
+#include "HeroProiectile.h"
+
+HeroProiectile::HeroProiectile(cocos2d::Scene scene, Sprite* heroSpaceShip, float speed, float scale)
+{
+    auto bullet = Sprite::create("res/laserGreen.png");
+    
+    bullet->setScale(scale);
+    bullet->setAnchorPoint(Vec2(0.5, 0.5));
+    bullet->setPosition(Vec2(heroSpaceShip->getBoundingBox().getMidX(),
+                             heroSpaceShip->getBoundingBox().getMaxY() + bullet->getBoundingBox().size.height/2));
+    
+    bullet->runAction(MoveTo::create(speed, Vec2(bullet->getBoundingBox().getMidX(),
+                                                           scene.getBoundingBox().size.height + bullet->getBoundingBox().size.height + 1)));
+    
+    this->addChild(bullet);
+}
---
+++ HeroProiectile.h
@@ -0,0 +1,22 @@
+//
+//  HeroProiectile.h
+//  Marsk
+//
+//  Created by Albastroiu Radu on 7/5/17.
+//
+//
+
+#pragma once
+#include "cocos2d.h"
+USING_NS_CC;
+
+class HeroProiectile : public cocos2d::Sprite
+{
+public:
+    HeroProiectile(cocos2d::Scene scene, Sprite* heroSpaceShip, float speed, float scale);
+    cocos2d::Sprite* GetProiectile();
+    
+private:
+    cocos2d::Sprite* proiectile;
+    
+};
