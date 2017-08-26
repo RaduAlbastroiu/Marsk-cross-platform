a8e22a54dcbd4abeae921f61de0f8b0e725f6494
Albastroiu Radu <albastroiuradug@gmail.com>
2017-08-26 04:16:59 +0300

    base header
    - contains the most important libraries

---
+++ Header.h
@@ -0,0 +1,30 @@
+//
+//  Header.h
+//  Marsk
+//
+//  Created by Albastroiu Radu on 8/26/17.
+//
+//
+
+#pragma once
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstdint>
+#include "cocos2d.h"
+
+using namespace std;
+
+USING_NS_CC;
+
